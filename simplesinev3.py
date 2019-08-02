e7b8ecfb0d4dc2516739b2efb4e6ebe282c802d6
sascha <sascha.lippert@gmail.com>
2019-08-02 02:39:52 +0200

    init

--- simplesinev3.py
+++ simplesinev3.py
@@ -19,14 +19,20 @@ class Model(nn.Module):
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.n_layers = n_layers
+        self.noise_mean = 0.0
+        self.noise_stddev = 0.1
 
         self.net = nn.LSTM(input_size, hidden_size, num_layers = n_layers, dropout=drop_prob, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, input, hidden):
+    def forward(self, input, hidden, add_noise = False):
         # x (batch_size, seq_length, input_size)
         # hidden (n_layers, batch_size, hidden_dim)
         # r_out (batch_size, time_step, hidden_size)
+
+        if(add_noise):
+            noise = input.data.new(input.size()).normal_(self.noise_mean, self.noise_stddev)
+            input = input + noise
 
         out, hidden = self.net(input, hidden)
         out = out[:,-1,:] #only last sequence is evaluated
@@ -138,7 +144,7 @@ def train_model(model, optimizer, criterion, n_epochs):
 
             optimizer.zero_grad()
 
-            out, hidden = model(inputs, hidden)
+            out, hidden = model(inputs, hidden, add_noise = True)
             hidden = tuple([each.data for each in hidden])
 
             loss = criterion(out, targets)
